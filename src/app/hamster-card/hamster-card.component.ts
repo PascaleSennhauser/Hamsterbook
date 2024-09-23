0eeff4ffaaefefa07137c9d9faa72d321c467797
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-09-24 00:16:10 +0200

    Added fonts

---
+++ src/app/hamster-card/hamster-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hamster-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './hamster-card.component.html',
+  styleUrl: './hamster-card.component.scss'
+})
+export class HamsterCardComponent {
+
+}
