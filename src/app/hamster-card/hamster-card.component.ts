16893d2def3a1f398e7ac7090a7cc32ab36008bd
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-09-26 12:48:52 +0200

    You can toggle the color of the favorite-icon

--- src/app/hamster-card/hamster-card.component.ts
+++ src/app/hamster-card/hamster-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hamster-card',
   standalone: true,
-  imports: [ MatIconModule ],
+  imports: [ MatIconModule, CommonModule ],
   templateUrl: './hamster-card.component.html',
   styleUrl: './hamster-card.component.scss'
 })
@@ -14,4 +15,13 @@ export class HamsterCardComponent {
     text: 'Hallo, mein Name ist Felix. Ich bin hier, um neue Freunde zu treffen!',
     img: 'assets/img/hamster/hamster-1.jpg'
   };
+  heartIsRed = false;
+
+  toggleColor() {
+    if (!this.heartIsRed) {
+      this.heartIsRed = true;
+    } else {
+      this.heartIsRed = false;
+    }
+  }
 }
